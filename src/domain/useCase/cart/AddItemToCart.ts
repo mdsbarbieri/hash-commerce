6390f7c6d3e669d03c61d9149c1210a96faab23c
Matheus Barbieri <mdsbarbieri@gmail.com>
2022-01-10 10:41:30 -0300

    chore: Segment getCart method on add item to cart

--- src/domain/useCase/cart/AddItemToCart.ts
+++ src/domain/useCase/cart/AddItemToCart.ts
@@ -11,15 +11,19 @@ export class AddItemToCart implements UseCase {
   constructor(private readonly productRepository: ProductRepository, private readonly shoppingCart: ShoppingCart, private readonly cartRepository: CartRepository) {
   }
 
-  async execute(cartRequest: CartRequest): Promise<Cart> {
-    let cart = await this.cartRepository.getById(cartRequest.cartId);
+  private async getCart(cartId: string): Promise<Cart>  {
+    let cart = await this.cartRepository.getById(cartId);
     if (!cart) {
-      cart = await this.cartRepository.set(cartRequest.cartId, {products: new Map<string, CartItem>()});
+      cart = await this.cartRepository.set(cartId, {products: new Map<string, CartItem>()});
     }
-
     if (!cart) {
       throw new BusinessError('Cannot create cart');
     }
+    return cart
+  }
+
+  async execute(cartRequest: CartRequest): Promise<Cart> {
+    let cart = await this.getCart(cartRequest.cartId);
 
     for (const cartItem of cartRequest.products) {
       const product = await this.productRepository.getById(cartItem.id?.toString());
